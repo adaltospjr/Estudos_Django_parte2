9f51df0b79b2ecc8f9d4eb0c970b5ec7e9779557
Adalto Linhares <adaltospjr@gmail.com>
2022-03-14 17:02:14 -0300

    Atualizações.

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,8 +1,11 @@
+from cProfile import label
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from passagens.classe_viagens import tipo_de_classe
 
 class PassagemForms(forms.Form):
     origem = forms.CharField(label = 'Origem', max_length=100)
     destino = forms.CharField(label = 'Destino', max_length=100)
     data_ida = forms.DateField(label = 'Ida', widget=DatePicker())
     data_volta = forms.DateField(label = 'Volta', widget=DatePicker())
+    classe_viagem = forms.ChoiceField(label='Classe do vôo', choices=tipo_de_classe)
