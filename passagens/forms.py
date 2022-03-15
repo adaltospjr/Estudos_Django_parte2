12c67b2b49ba5749a87fcc3284be77a729f064d1
Adalto Linhares <adaltospjr@gmail.com>
2022-03-15 09:51:32 -0300

    Adicionando novos campos no form.

--- passagens/forms.py
+++ passagens/forms.py
@@ -2,10 +2,19 @@ from cProfile import label
 from django import forms
 from tempus_dominus.widgets import DatePicker
 from passagens.classe_viagens import tipo_de_classe
+from datetime import datetime
 
 class PassagemForms(forms.Form):
     origem = forms.CharField(label = 'Origem', max_length=100)
     destino = forms.CharField(label = 'Destino', max_length=100)
     data_ida = forms.DateField(label = 'Ida', widget=DatePicker())
     data_volta = forms.DateField(label = 'Volta', widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Data de pesquisa', disabled=True, initial=datetime.today)
     classe_viagem = forms.ChoiceField(label='Classe do vôo', choices=tipo_de_classe)
+    informacoes = forms.CharField(
+        label='Informações extras',
+        max_length=200,
+        widget=forms.Textarea(),
+        required=False
+    )
+    email = forms.EmailField(max_length=100)
