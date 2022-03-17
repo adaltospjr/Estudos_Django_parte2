7e1e7b88d419ca82fb5c8fde3ad71a47d83dde23
Adalto Linhares <adaltospjr@gmail.com>
2022-03-17 17:21:45 -0300

    Continuação dos tratamentos.

--- passagens/validation.py
+++ passagens/validation.py
@@ -16,4 +16,4 @@ def data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros):
 def data_ida_menor_data_de_hoje(data_ida, data_pesquisa, lista_de_erros):
     """Verifica se data de ida é menor que data de hoje """ 
     if data_ida < data_pesquisa:
-        lista_de_erros['data_ida'] = 'Data de ida não pode ser menor que hoje' 
+        lista_de_erros['data_ida'] = 'Data de ida não pode ser menor que hoje'
